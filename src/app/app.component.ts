0bbd095b0cb62f8fdde1cded11e5eac33ee67f96
IEVN1003-19002151 <sainzfabian01@gmail.com>
2024-11-14 10:17:51 -0600

    serviciosFinal 13/11/24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TempComponent } from "./tem/temp/temp.component";
 import { NavbarComponent } from "./navbar/navbar.component";
-import { AddMessageComponent } from './tem/add-message/add-message.component';
-import { ListMessageComponent } from "./tem/list-message/list-message.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TempComponent, NavbarComponent, AddMessageComponent, ListMessageComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
